0369c31416d426c8d42b7c73f508640144a9cbed
hj-lab <siannyu@naver.com>
2020-08-08 23:32:25 +0900

    index 및 calc 수정

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,16 @@
 $(document).on('click', '#btn-calc', function() {
-    alert('Hello!');
+    $.ajax({
+        url: "https://maplestory.nexon.com/Ranking/World/Total?c=%EC%95%84%EB%AF%B8%EC%B8%A0",
+        dataType: "html",
+        success: function (data) {
+            console.log(data);
+            // $('#content').html(data);
+            $(data).find('아미츠').each(function() {
+                console.log($(this));
+            })
+        },
+        error: function(data) {
+            console.log('failed');
+        }
+    });
 });
