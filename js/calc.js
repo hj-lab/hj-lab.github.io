fdc2b015cf6240b5c2ada986188a0f2a0bb7a7ed
hj-lab <siannyu@naver.com>
2020-08-11 01:56:26 +0900

    index, calc 수정

--- js/calc.js
+++ js/calc.js
@@ -1,60 +1,56 @@
-$(document).on('click', '#btn', function () {
-    // history.replaceState(null, null, '#https://www.naver.com')
+var check_name = [],
+    $test;
 
-    $.ajax({
-        url: "https://maplestory.nexon.com/Common/Resource/Item?p=ESiGOaxye8ZazwJEjUhfLFPWF%2fqbF1XfG8ljVaYDLdh5gAOAYkDMNiwntEZOS8u7GrHI0jfzHf1hJpLghIMUqm3v4pXPvJtK4Mh2jBYCDLaeSz5ERvTxeXFrjuzelM9%2bAv8d8XP4RdgnSZYVh7Qik9Vq6%2fmQJeWdWKxugGNz8w0d2E0owxBOW%2bPdgny0ZmsZ32swAR3FWaLgX5%2fjSJSoupr01vXZKABBYVOasgaGIdSvHFbnGJ7mVaz4yeHVCANx%2fwCZCWvXQt4HgTV%2f6HQu1mjH74wmTHChy4JbfocJS1ISSXTHuPD%2bUlHSPcloDcy49PLBxRURP%2bsxDVolL7%2bAjw%3d%3d",
-        type: 'GET',
-        async: false,
-        cache: false,
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (data) {
-            console.log('failed');
-        }
-    });
+// 입력박스 클릭시 초기회
+$(document).on('click', '#name', function() {
+    $('#name').val('');
 });
 
-var check_nick = [];
+// 이름 추가
 $(document).on('click', '#btn-add', function() {
-    var name = $('#name').val();
-    if(check_nick.includes(name)) {
+    var name = $('#name').val(),
+        content = '';
+
+    if(check_name.includes(name)) {
         alert('이미 등록되어있습니다.');
     } else if(name == '') {
         alert('비어있습니다.');
     } else {
-        $('#id').append('<button href="#" class="list-group-item list-group-item-action nick" data-toggle="list">' + name + '</button>');
-        check_nick.push(name);
+        content += '<button class="list-group-item list-group-item-action name" data-toggle="list" data-name="' + name + '">';
+        content += name;
+        content += '<a href="#" class="badge badge-danger badge-primary del">삭제</a>'
+        content += '</button>'
+        $('#id').append(content);
+        check_name.push(name);
     }
 });
 
-$(document).on('click', '.nick', function() {
-    var nick = $(this).text();
-    console.log(nick);
-    one(nick);
+// 이름 삭제
+$(document).on('click', '.del', function(e) {
+    e.stopPropagation();
+    $(this).parents('button').remove();
 });
 
-$('#name').keyup(function (key) {
-    if (key.keyCode == 13) {
-        alert('a');
-    }
+// 이름 선택
+$(document).on('click', '.name', function(e) {
+    var name = $(this).attr('data-name');
+    search_char(name);
 });
 
-var $test;
-
-function one(nick) {
+// 캐릭터 찾기
+function search_char(name) {
     var base = 'https://maplestory.nexon.com',
         flag = true;
 
     $.ajax({
-        url: "https://maplestory.nexon.com/Ranking/World/Total?c=" + nick,
+        url: "https://maplestory.nexon.com/Ranking/World/Total?c=" + name,
         dataType: "html",
         success: function (data) {
             $(data).find('dt').each(function () {
                 var $tmp = $(this)[0];
 
                 // console.log($tmp.innerText);
-                if ($tmp.innerText == nick) {
+                if ($tmp.innerText == name) {
                     flag = false;
                     address = $(this).find('a')[0].attributes[0].value;
                     // console.log(address);
@@ -62,7 +58,7 @@ function one(nick) {
                         str2 = address.substr(address.indexOf('?'), address.length),
                         marge = str1 + '/Equipment' + str2;
                     // console.log(marge);
-                    two(base + marge);
+                    load_item(base + marge);
                 }
             });
             if(flag) {
@@ -75,22 +71,14 @@ function one(nick) {
     });
 }
 
-$(document).on('click', '#name', function() {
-    $('#name').val('');
-})
-
-$(document).on('click', '#btn-delete', function() {
-    $('#테이').remove();
-    // $(this).remove();
-})
-
-function two(url) {
+// 아이템 불러오기
+function load_item(url) {
     $.ajax({
         url: url,
         dataType: 'html',
         success: function (data) {
-            var $op = $(data).find('ul.item_pot > li');
-            console.log($op);
+            // var $op = $(data).find('ul.item_pot > li'); // 아이템 옵션
+            // console.log($op);
             // console.log($(data).find('div.char_img > div > img'));
             $('#dd').html('<img src="' + $(data).find('div.char_img > div > img')[0].src + '" alt="캐릭터">');
             $test = $(data).find('ul.item_pot > li > img')
@@ -104,12 +92,11 @@ function two(url) {
                         content += '<li></li>';
                         continue;
                     } else if($test[i - m].alt == '') {
-                        // content += '<li></li>';
+                        content += '<li></li>';
                         continue;
                     }
-                    // console.log(test[i - m].alt);
-                    // content += '<li><img src="' + $test[i - m].src + '" alt="' + $test[i - m].alt + '"></li>';
-                    content += $op[i-m].outerHTML;
+                    // console.log($test[i - m].alt);
+                    content += '<li><img src="' + $test[i - m].src + '" alt="' + $test[i - m].alt + '"></li>';
                 } catch(e) {
                     console.log(e.name);
                     if(e.name === 'TypeError') {
@@ -119,8 +106,9 @@ function two(url) {
                 }
             }
             $('.item_pot').html(content);
-        }, error: function (data) {
-            console.log('two failed');
-        }
+        }, error: function () {
+            console.log('failed');
+        },
+        timeout: 3000
     });
 }
